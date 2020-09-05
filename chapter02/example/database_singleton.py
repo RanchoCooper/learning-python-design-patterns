4a7d91f8be4c564a055135653f7a9acedff3bc34
Rancho <ranchocooper@gmail.com>
2020-09-06 01:58:56 +0800

    singleton example: database

---
+++ chapter02/example/database_singleton.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import sqlite3
+
+
+class MetaSingleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Database(metaclass=MetaSingleton):
+    connection = None
+
+    def connect(self):
+        if self.connection is None:
+            self.connection = sqlite3.connect("db.sqlite3")
+            self.cursorobj = self.connection.cursor()
+        return self.cursorobj
+
+
+if __name__ == '__main__':
+    db1 = Database().connect()
+    db2 = Database().connect()
+    print("database object db1: ", db1)
+    print("database object db2: ", db2)
